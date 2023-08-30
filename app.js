b1fce7da76a57909a0fb85e8998a5453064764ee
Romzi <romzifg@gmail.com>
2023-08-30 23:09:41 +0700

    update, add morgan for middleware logging request

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const CategoriesRouter = require('./routes/categories')
+const CategoriesRouter = require('./routes/categories');
+const morgan = require('morgan');
 require('dotenv').config()
 
 // Middleware
 app.use(express.json())
+app.use(morgan("dev"))
 
+// Routing
 app.use('/api/v1/categories', CategoriesRouter)
 
 app.listen(process.env.PORT, () => {
