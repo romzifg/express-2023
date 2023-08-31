752cd7fb81fd0de6a8235e44e68074f1daf59e09
Romzi <romzifg@gmail.com>
2023-08-31 08:46:59 +0700

    update, setup sequelize, mysql and cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const CategoriesRouter = require('./routes/categories');
 const morgan = require('morgan');
+const cors = require('cors');
 require('dotenv').config()
 
 // Middleware
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(cors())
 
 // Routing
 app.use('/api/v1/categories', CategoriesRouter)
