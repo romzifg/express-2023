88a4c68ea4364577fc317b2921cbb479ae950224
Romzi <romzifg@gmail.com>
2023-09-12 20:46:32 +0700

    update, add coupon model and migration

---
+++ migrations/20230912134115-create-coupon.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Coupons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      code: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      expired_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      disc_value: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Coupons');
+  }
+};
---
+++ models/coupon.js
@@ -0,0 +1,66 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Coupon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Coupon.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'coupon name has been used'
+      },
+      validate: {
+        notNull: {
+          msg: 'coupon name cannot empty'
+        }
+      }
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'coupon code has been used'
+      },
+      validate: {
+        notNull: {
+          msg: 'coupon code cannot empty'
+        }
+      }
+    },
+    expired_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'coupon expired date cannot empty'
+        }
+      }
+    },
+    disc_value: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'coupon discon value cannot empty'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Coupon',
+  });
+  return Coupon;
+};
